58203836e8bf9bea78d5b98a2733c56e71e90ed5
Absiinator <jeffreylepage51@gmail.com>
2022-05-19 10:07:06 +0200

    initial commit

--- Leaderboard/settings.py
+++ Leaderboard/settings.py
@@ -20,11 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# get key from heroku
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-6q80o1br$o5n7rv&j3hq(&90*&knz((dad612pj=bk2(w-c+w='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
